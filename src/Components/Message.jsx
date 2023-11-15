50a5cf4426861eeb4beedab373e386d7e489a4b2
saifularafat <saifularafat.info@gmail.com>
2023-11-16 02:17:47 +0600

    register site and firebase config

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,8 +1,17 @@
 
 const Message = () => {
     return (
-        <div>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, voluptate.</h2>
+        <div className="flex gap-2 mb-3 owner">
+            <div className="flex-col text-green-400 font-medium">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQUXqMrzrmkxd3QpxGL5bzgxELsztrL1AgQ&usqp=CAU"
+                    alt="" className="w-10 h-10 rounded-full object-cover" />
+                <p className="text-sm">just now</p>
+            </div>
+            <div className="max-w-[80%] flex-col gap-2 space-y-1 ownerContent">
+                <p className="bg-white max-w-max py-2 px-3 rounded-tr-lg rounded-br-lg rounded-bl-xl mt-2 text-sm font-normal">Hello Brother</p>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQUXqMrzrmkxd3QpxGL5bzgxELsztrL1AgQ&usqp=CAU" alt="" className="w-4/5"/>
+            </div>
+
         </div>
     );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,15 +1,67 @@
 import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../../firebase.config";
+import {
+    ref,
+    uploadBytesResumable,
+    getDownloadURL
+} from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
+import { useState } from "react";
 const Register = () => {
-
-    const handleSubmit =(e) => {
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const handleSubmit = async (e) => {
+        setLoading(true)
         e.preventDefault();
         const form = e.target
         const displayName = form.displayName.value;
         const email = form.email.value;
         const password = form.password.value;
-        const photoUrl = form.photoUrl.value;
-        console.log(displayName, email, password, photoUrl);
+        const file = form.photoUrl.file;
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(res);
+            const storageRef = ref(storage, displayName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+
+                (error) => {
+                    setError(true)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        });
+                        await setDoc(doc(db, 'users', res.user.uid), {
+                            uid: res.user.uid,
+                            displayName, 
+                            email,
+                            password, 
+                            photoURL: downloadURL,
+                        })
+                    });
+
+                }
+            );
+        } catch (error) {
+            setError(true)
+            setLoading(false)
+        }
+        // .then((userCredential) => {
+        //     // Signed up 
+        //     const user = userCredential.user;
+        //     // ...
+        // })
+        // .catch((error) => {
+        //     const errorCode = error.code;
+        //     const errorMessage = error.message;
+        //     // ..
+        // });
     }
     return (
         <div className="flex items-center justify-center bg-blue-400 h-screen">
@@ -19,7 +71,7 @@ const Register = () => {
                     <p className="text-base font-medium">Register</p>
                 </div>
                 <form onSubmit={handleSubmit}
-                className="flex-col items-center space-y-4">
+                    className="flex-col items-center space-y-4">
                     <input type="text" name="displayName" id="" placeholder="display your name" required className="input_filed capitalize" /> <br />
                     <input type="email" name="email" id="" placeholder="your email" required className="input_filed lowercase" /><br />
                     <input type="password" name="password" id="" placeholder="*******" required className="font-normal input_filed" /><br />
@@ -30,6 +82,8 @@ const Register = () => {
                     </label>
                     <input type="submit" value="Submit" className="w-full bg-blue-700 rounded-lg py-2 px-3 text-white text-xl font-medium cursor-pointer" />
                 </form>
+                {loading && <span className="text-blue-600 ">Loading..</span>}
+                {error && <span className="text-red-600 ">something is error..</span>}
                 <p className="text-sm">You do have an account? <Link to="/login" className="text-sm font-serif font-medium text-blue-800 hover:text-red-800">Login</Link></p>
             </div>
         </div>
